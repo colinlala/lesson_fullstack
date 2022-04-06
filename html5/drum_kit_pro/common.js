64998bde1671774fb19ccf0d89d18a456cbfcfe5
colincclala <1522302196@qq.com>
2022-04-06 13:41:47 +0000

    pro

---
+++ html5/drum_kit_pro/common.js
@@ -0,0 +1,37 @@
+// keydown even name -> window
+window.addEventListener('keydown',function(event){
+    // 键值
+    let keyCode = event.keyCode;  //  按键事件的数据
+    let audioEle = 
+        document.querySelector(`audio[data-key="${keyCode}"]`);
+    // console.log(audioEle);
+    if(!audioEle)return;
+    audioEle.play();
+    let keyEle = 
+        document.querySelector(`.key[data-key="${keyCode}"]`);
+    if(!keyEle)return;
+
+    keyEle.classList.add('playing');
+    // 定时器 1000ms
+    // setTimeout(function(){
+    //     // console.log('---------------');
+    //     keyEle.classList.remove('playing');
+    // }, 500)
+
+    // console.log(keyCode);
+
+})
+
+
+
+const keys = document.querySelectorAll('.key');
+for(let key of keys){
+    // console.log(key);
+    // event 一个个加，不能给集体加
+    // 当transitionend过渡效果完成后执行一个function
+    key.addEventListener('transitionend', function(event){
+        // 不同类型的事件 event对象也是不同的
+        // console.log(event , '------------------')
+        event.target.classList.remove('playing');
+    })
+}
---
+++ html5/drum_kit_pro/readme.md
@@ -0,0 +1,8 @@
+# css 预编译器
+
+- 假如写css时，有一个emmet快捷输入并tab完成的工具。
+- npm ：node package management node包管理器
+    下载组件
+- css 预编译工具 属于npm世界
+    i install
+    -g 全局安装
