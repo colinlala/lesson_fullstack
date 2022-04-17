6e49d67304c60940fd2cb52f770ba882df45082f
colincclala <1522302196@qq.com>
2022-04-17 04:55:51 +0000

    14 最长公共前缀

---
+++ leetcode/14_longest_public_prefix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    if(strs.length == []){
+        return ""
+    }else{
+        let res = ""
+        let max = strs[0]
+        let min = strs[0]
+        for(var i = 0;i<strs.length-1;i++){
+            min = min > strs[i+1] ? strs[i+1] : min
+            max = max < strs[i+1] ? strs[i+1] : max
+            //找到数组中字符串的最大和最小值
+        }
+        //  比较最大和最小的公共前缀
+        for(var x = 0;x<max.length;x++){
+            if(max[x] == min[x]){
+                res = res + max[x]
+            }else{
+                break
+            }
+        }
+        return res
+    }
+};
