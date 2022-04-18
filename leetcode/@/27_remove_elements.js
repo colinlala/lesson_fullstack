003658fe655bf674849f7049221b3db4b916a87d
colincclala <1522302196@qq.com>
2022-04-18 16:39:31 +0000

    27 移除元素

---
+++ leetcode/@/27_remove_elements.js
@@ -0,0 +1,36 @@
+// 移除元素
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+ var removeElement = function(nums, val) {
+    let index = 0, last = nums.length - 1
+    while (index <= last) {
+        if (nums[index] === val) {
+            nums[index] = nums[last]
+            last--
+        } else {
+            index++
+        }
+  }
+  return index
+
+};
+// let n = [1,2,2,3,4]
+// console.log(removeElement(n,2),n)
+
+
+//  var removeElement = function(nums, val) {
+//     let left = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] == val){
+//             nums.splice(i,1);
+//             i--;
+//         }
+//         else{
+//             left++;
+//         }
+//     }
+//     return left;
+// };
