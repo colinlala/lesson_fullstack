c3961e44eb2e46c9b421f727ea01e467d721d10d
colincclala <1522302196@qq.com>
2022-04-18 07:16:48 +0000

    慕课js  课程稍微有点老，新部分参考books->es6入门

---
+++ study_by_myself/js/array/1.js
@@ -0,0 +1,29 @@
+// let arr = [12,33,43,53,67,54]
+// console.log(arr.sort())
+// console.log(arr)//[ 12, 33, 43, 53, 54, 67 ]
+
+let arr1 = ['a','d','b','c']
+console.log(arr1.sort())
+console.log(arr1)//[ 'a', 'b', 'c', 'd' ]
+
+let arr2 = ['大','dai','代','搭']
+console.log(arr2.sort())
+console.log(arr2)//[ 'dai', '代', '大', '搭' ]
+
+
+let arr3 = ['大','大小','大大大','大大']
+console.log(arr3.sort())
+console.log(arr3)//[ '大', '大大', '大大大', '大小' ]
+
+
+var arr = [1, 2, 3, 4, 5];
+arr.slice(1, 3); // [2, 3]
+arr.slice(1); // [2, 3, 4, 5]
+arr.slice(1, -1); // [2, 3, 4]
+arr.slice(-4, -3); // [2]
+console.log(arr.slice(-5, -3))
+console.log(arr.some(function(x) {
+    return x === 123;
+}))
+
+
