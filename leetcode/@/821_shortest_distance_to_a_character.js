de13c1f27db026d11b39c8fe1307efb1953d21ea
colincclala <1522302196@qq.com>
2022-04-19 02:38:43 +0000

    821 最短字符距离

---
+++ leetcode/@/821_shortest_distance_to_a_character.js
@@ -0,0 +1,20 @@
+// 字符的最短距离
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+ var shortestToChar = function(s, c) {
+    let array = [];
+    for (let i = 0; i < s.length; i++) {
+        array[i] = s.length;
+    }
+    for (let i = 0; i < s.length; i++){
+        if ( s.substring(i, i+1) === c){
+            for (let j = 0; j < s.length; j++){
+                array[j] = Math.min(Math.abs(i - j),array[j])
+            }
+        }
+    }
+    return array;
+};
