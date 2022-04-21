40c9c8460cea0d1d4c5d0b45c702f08188438fcf
colincclala <1522302196@qq.com>
2022-04-21 09:14:32 +0000

    819 最常见单词

---
+++ leetcode/@/819_common_words.js
@@ -0,0 +1,16 @@
+// 最常见单词
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+ var mostCommonWord = function(paragraph, banned) {
+    const temp = paragraph.toLowerCase().split(/\W+/)
+    let map = new Map()
+    for(let char of temp){
+        if(!banned.includes(char))
+        map.set(char,(map.get(char) || 0) + 1)
+    }
+    const arr = [...map].sort((a,b) => b[1]-a[1])
+    return arr[0][0]
+};
