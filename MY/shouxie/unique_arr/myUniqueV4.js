2b5d70a1b25b36093b62e8c01d0782589677b90a
colincclala <1522302196@qq.com>
2022-04-22 10:59:32 +0000

    数组去重 indexOf或includes

---
+++ MY/shouxie/unique_arr/myUniqueV4.js
@@ -0,0 +1,18 @@
+let array = [1,2,2, 1, "1", "1", true, true, "true", {}, {}, "{}", null, null, undefined, undefined];
+/**
+ * @func 数组去重 
+ * @params  arr 数组
+ * @returns array
+*/
+function myUnique(arr){
+    let brr = []
+  arr.forEach(item => {
+    // 使用 indexOf  返回数组是否包含某个值 没有就返回 -1 有就返回下标
+    if(brr.indexOf(item) === -1) brr.push(item);
+    // 或使用 includes 返回数组是否包含某个值  没有就返回false  有就返回true
+    // if(!brr.includes(item)) brr.push(item);
+  })
+  return brr;
+}
+
+console.log(myUnique(array));
