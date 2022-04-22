a70c3d0be88f4f774ba049a64677f4894f98b264
colincclala <1522302196@qq.com>
2022-04-22 10:56:40 +0000

    数组去重   Set生成数组

---
+++ MY/shouxie/unique_arr/myUniqueV1.js
@@ -0,0 +1,15 @@
+let array = [1,2,2, 1, "1", "1", true, true, "true", {}, {}, "{}", null, null, undefined, undefined];
+/**
+ * @func 数组去重 
+ * @params  arr 数组
+ * @returns array
+*/
+function myUnique(arr){
+    // 用Set生成数组 会自动去重。
+    let array = new Set(arr);
+    // Array.from() 方法对一个类似数组或可迭代对象创建一个新的，浅拷贝的数组实例。
+    let cur = Array.from(array);
+    return cur;
+}
+    
+console.log(myUnique(array));
