b7271d83ebe5041296be25f88f06ef9ebed3398e
colincclala <1522302196@qq.com>
2022-04-25 14:10:05 +0000

    374 猜数字大小

---
+++ leetcode/BinarySearch/374_guess_number.js
@@ -0,0 +1,37 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-25 22:08:54
+ * @LastEditTime: 2022-04-25 22:08:55
+ * @Description: 猜数字大小
+ * 
+ */
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+    let low = 1;
+    let high = n;
+    while (low <= high) {
+        const mid = Math.floor((low + high) / 2);
+        const res = guess(mid);
+        if (res === 0) {
+            return mid;
+        } else if (res === 1) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+};
