8c9b40bf0df3caddacf457ff5ed7c73e4465fecb
colincclala <1522302196@qq.com>
2022-04-25 08:16:37 +0000

    217 存在重复元素

---
+++ leetcode/@/217_contains_duplicate.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-25 16:15:36
+ * @LastEditors: ZYH
+ * @LastEditTime: 2022-04-25 16:15:36
+ * @Description: 存在重复元素
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    // Size属性将会返回Set对象中元素的个数。
+    let newArr = new Set(nums)
+    if (newArr.size === nums.length)
+        return false;
+    else
+        return true;
+}
