fe03ca2bbeb51e054616728f9f5cb38eb44c715e
colincclala <1522302196@qq.com>
2022-04-25 07:43:21 +0000

    1365 有多少小于当前数字的数字

---
+++ leetcode/@/1365_small_num_cur.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-25 15:41:18
+ * @LastEditors: ZYH
+ * @LastEditTime: 2022-04-25 15:41:18
+ * @Description: 有多少小于当前数字的数字
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function (nums) {
+    let temp = nums.slice();
+    let res = [];
+    // 升序
+    temp.sort((a, b) => a - b)
+    for (let num of nums) {
+        res.push(temp.indexOf(num));
+    }
+    return res;
+};
