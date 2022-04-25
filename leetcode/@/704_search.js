5d87ad442f6b6c527977617c9d68806f7d51c4c1
colincclala <1522302196@qq.com>
2022-04-25 14:05:41 +0000

    704 二分查找

---
+++ leetcode/@/704_search.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-25 22:04:36
+ * @LastEditTime: 2022-04-25 22:04:37
+ * @Description: 二分查找
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    var min = 0
+    var max = nums.length - 1
+    while (min <= max) {
+        // Math.floor() 返回小于或等于一个给定数字的最大整数。
+        var mid = Math.floor((min + max) / 2);
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            max = mid - 1;
+        } else if (target > nums[mid]) {
+            min = mid + 1;
+        }
+    }
+    return -1;
+};
