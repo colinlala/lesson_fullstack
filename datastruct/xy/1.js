74646775cef3d3815503fb23b96bfabf2254aed8
colincclala <1522302196@qq.com>
2022-04-25 13:31:49 +0000

    怎么创建数组

---
+++ datastruct/xy/1.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-25 19:34:37
+ * @LastEditTime: 2022-04-25 19:58:53
+ * @Description:
+ *
+ */
+// 怎么创建数组？
+// 数据结构，代码的优化是有意义的，平时开发算法用得少，针对面试学习
+const arr = [];  // easy way，length没定，items类型也没有定
+
+// 使用构造函数，可不是为了创建空数组这么无聊
+// 数组比较易用，相对于链表，默认用数组来实现
+const arr2 = new Array(7);     // <7 empty items>  item类型？
+// console.log(arr2);
+
+
+
+const arr3 = (new Array(7)).fill(1);
+
+console.log(arr3);
