c0a5b496ac9452a06b617d871a8db66a219d1c36
colincclala <1522302196@qq.com>
2022-04-26 09:34:20 +0000

    35 查找插入位置

---
+++ leetcode/BinarySearch/35_search_insertion_position.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-26 17:28:25
+ * @LastEditTime: 2022-04-26 17:28:25
+ * @Description: 查找插入位置
+ * 
+ */
+// 查找插入位置
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    const n = nums.length;
+    let left = 0, right = n - 1, ans = n;
+    while (left <= right) {
+        let mid = ((right - left) >> 1) + left;
+        if (target <= nums[mid]) {
+            ans = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return ans;
+};
