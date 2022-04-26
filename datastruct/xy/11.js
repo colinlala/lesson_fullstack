830dc6bca8264f6887e9beb2dbcf112d3aa717b9
colincclala <1522302196@qq.com>
2022-04-26 14:41:25 +0000

    直觉？用空间（廉价）换时间（值钱）

---
+++ datastruct/xy/11.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-26 22:13:58
+ * @LastEditTime: 2022-04-26 22:31:25
+ * @Description:用空间（廉价）换时间（值钱）
+ *
+ */
+
+// 数组在leetcode  题目  两数相加
+// 数组相关，算法？排序（冒泡 两重for，插入），二分算法（有效提升效率），动态规划
+// 冒泡   暴力解决  淳朴  两重for
+// 1.基本思路？两层循环
+// 2.反思？算法超时？o(n^2)  直觉？用空间（廉价）换时间（值钱），优化成一层循环
+// [] o(1)     {} key:val o(1)
+const nums = [2, 7, 11, 15];
+const target = 9;
+let obj = {};  // key val
+for (let i = 0; i < nums.length; i++) {
+    // nums[i]  2,7,11,15
+    // 差的算法   target - nums[i] -> obj 里面 -> 2 key?
+
+}
+
---
+++ datastruct/xy/12.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-26 22:31:30
+ * @LastEditTime: 2022-04-26 22:36:53
+ * @Description: 
+ * 
+ */
+
+/**
+ * @description: 
+ * @param {*} nums
+ * @param {*} target
+ * @return {*}
+ */
+const twoSum = function (nums, target) {
+    // 用空间换时间，减少一次循环
+    const diffs = {};
+    // 缓存数组长度
+    const len = nums.length;
+    for (let i = 0; i < len; i++) {
+        if (diffs[target - nums[i]] !== undefined) {
+            return [diffs[target - nums[i]], i];
+        } else {
+            diffs[nums[i]] = i;
+        }
+    }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
