6889ef1b8844a9f83ff0d6ffd817ee563b1707df
colincclala <1522302196@qq.com>
2022-04-26 09:43:02 +0000

    141 环形链表 快慢指针

---
+++ leetcode/64/141_has_cycle.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-26 17:41:53
+ * @LastEditTime: 2022-04-26 17:41:53
+ * @Description: 环形链表 快慢指针
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+//  如果有环始终会追上
+var hasCycle = function (head) {
+    let fast = slow = head;
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if (fast == slow) {
+            return true;
+        }
+    }
+    return false;
+};
