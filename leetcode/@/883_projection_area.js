f34cbcf6e4fd3798b07c3aaea9c08daed87cd5ce
colincclala <1522302196@qq.com>
2022-04-26 02:04:38 +0000

    883 三维形体投影面积

---
+++ leetcode/@/883_projection_area.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-26 10:03:18
+ * @LastEditTime: 2022-04-26 10:03:55
+ * @Description: 三维形体投影面积
+ * 
+ */
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var projectionArea = function (grid) {
+    let left = 0;
+    let right = 0;
+    let up = 0;
+    let n = grid.length;
+    for (let i = 0; i < n; i++) {
+        left += Math.max(...grid[i])
+        let temp = []
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] !== 0) {
+                up++
+            }
+            temp.push(grid[j][i])
+        }
+        right += Math.max(...temp)
+    }
+    return left + right + up
+
+};
