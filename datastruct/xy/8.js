031c4d0521e18da48a0ce5bd57b4404ec9fa08fc
colincclala <1522302196@qq.com>
2022-04-26 14:40:03 +0000

    栈 队列关系

---
+++ datastruct/xy/8.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-26 21:45:15
+ * @LastEditTime: 2022-04-26 21:49:51
+ * @Description:
+ *
+ */
+// 栈 队列？ 线性？
+// push shift FIFO
+
+const queue = []; // 数组表达队列
+queue.push('小测一节');
+queue.push('小测二节');
+queue.push('小测三节');
+
+while (queue.length) {
+    const top = queue[0];  // 队头出列
+    console.log('目前在排队的是:', top);
+    queue.shift();
+}
