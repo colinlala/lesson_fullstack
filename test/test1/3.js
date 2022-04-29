5286f1acb74e8c1bcc8108110591f2a8e2e7b3e1
colincclala <1522302196@qq.com>
2022-04-29 09:04:55 +0000

    ==先类型转换，再去比较、===类型也要求一样

---
+++ test/test1/3.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-04-29 16:15:14
+ * @LastEditTime: 2022-04-29 16:32:12
+ * @Description:  ==先类型转换，再去比较、===类型也要求一样
+ * 
+ */
+// == 类型转换 值相等就行 
+console.log(0 == 0); // true
+console.log(-0 == +0); // true
+console.log(-0 === +0); // true
+console.log(1 == '1'); // true 先类型转换'1'，再去比较
+console.log(1 === '1'); // false
+console.log(null == undefined); // true 同时转false
+console.log(null === undefined); // false 
+console.log({} == {}); // false  简单数据类型比较
+
+
+
