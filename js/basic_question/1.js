6d36ecc9bf11d5c5538c9df8dc25ab3c2c4b58fb
colincclala <1522302196@qq.com>
2022-05-04 15:51:21 +0000

    引用数据类型，传递对对象的引用，会影响

---
+++ js/basic_question/1.js
@@ -0,0 +1,34 @@
+/**
+ * @description: 
+ * @param {object} person
+ * @return {*}
+ */
+//  传值，简单数据类型   不会影响外面的
+// 引用数据类型，传递对对象的引用，会影响
+function test(person) {
+
+    let obj = {
+        ...person
+    }
+
+    obj.age = 26
+
+    // person.age = 26;  // 可读性不好
+    // // 把person 改成了新对象的地址
+    // person = {
+    //     name: 'yyy',
+    //     age: 30
+    // }
+    // return person
+}
+// 对象  堆内存中  地址
+const p1 = {
+    name: 'zyh',
+    age: 22
+}
+console.log(p1)    // { name: 'zyh', age: 22 }
+const p2 = test(p1)
+
+// console.log(p1)   // { name: 'zyh', age: 26 }
+console.log(p1)     //  { name: 'zyh', age: 22 }
+console.log(p2)   // { name: 'yyy', age: 30 }
