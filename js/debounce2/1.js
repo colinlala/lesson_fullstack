1d67a0a0c4a7960c76341d11ecd2c8078a623491
colincclala <1522302196@qq.com>
2022-05-17 02:21:43 +0000

    反转字符串api

---
+++ js/debounce2/1.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-17 09:33:43
+ * @LastEditTime: 2022-05-17 09:35:38
+ * @Description: 
+ */
+const str = 'zyh';
+const res = str.split('').reverse().join('');   // hyz
+console.log(res)
