8d459776cc999f0fa7dc541eb56ccb1e682bc94c
colincclala <1522302196@qq.com>
2022-05-23 06:30:39 +0000

    css变量、使用对象引入

---
+++ react/comments/src/comments/assets/css_variable.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-23 12:05:29
+ * @LastEditTime: 2022-05-23 12:06:52
+ * @Description: css变量
+ */
+export default {
+    'white_color': '#ffffff'
+}
