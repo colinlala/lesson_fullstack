55b98dc38c22fa1d658edd2f532fc73ea655799d
colincclala <1522302196@qq.com>
2022-05-23 06:27:39 +0000

    Comment组件

---
+++ react/comments/src/comments/Comment.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-23 10:42:42
+ * @LastEditTime: 2022-05-23 11:16:41
+ * @Description: Comment组件
+ */
+import React from 'react';
+
+function Comment() {
+    return (
+        <div>Comment</div>
+    )
+}
+
+export default Comment;
