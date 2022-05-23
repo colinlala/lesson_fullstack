be47c873780c05843c8785adf4695d1491f580cc
colincclala <1522302196@qq.com>
2022-05-23 06:26:25 +0000

    1

---
+++ react/comments/src/App.jsx
@@ -0,0 +1,22 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-22 23:05:28
+ * @LastEditTime: 2022-05-23 11:36:52
+ * @Description: 
+ */
+import { useState } from 'react'
+import CommentApp from './comments/CommentApp'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <CommentApp />
+    </div>
+  )
+}
+
+export default App
