f39d552e34fccdc4a244e0d862b4cf86f3bdb07e
colincclala <1522302196@qq.com>
2022-05-23 06:29:34 +0000

    CommentList组件

---
+++ react/comments/src/comments/CommentList.jsx
@@ -0,0 +1,24 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-23 10:42:24
+ * @LastEditTime: 2022-05-23 11:39:22
+ * @Description: CommentList组件
+ */
+import React from 'react';
+import Comment from './Comment';
+
+function CommentList() {
+    return (
+        <div>CommentList
+            <Comment />
+            <Comment />
+            <Comment />
+            <Comment />
+            <Comment />
+        </div>
+    )
+}
+
+export default CommentList;
