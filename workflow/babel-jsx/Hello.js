8ac003974ca58ef70bf71103928b3cb337f1e416
colincclala <1522302196@qq.com>
2022-05-24 08:46:33 +0000

    转义

---
+++ workflow/babel-jsx/Hello.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-23 20:59:14
+ * @LastEditTime: 2022-05-23 21:03:59
+ * @Description: 
+ */
+function Hello() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "box"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "title"
+  }, "Hello"), /*#__PURE__*/React.createElement("button", null, "do click"));
+}
---
+++ workflow/babel-jsx/Hello.jsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-23 20:59:14
+ * @LastEditTime: 2022-05-23 21:19:21
+ * @Description: 
+ */
+function Hello() {
+    return (
+        // JSX 是一个DOM对象
+        <div className="box" id="ele">
+            <div className="title">
+                Hello
+            </div>
+            <button>do click</button>
+        </div>
+    )
+}
