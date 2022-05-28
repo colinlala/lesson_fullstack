d8ca9b368b57e721c6df4b685bbe6fda4d2c35b9
colincclala <1522302196@qq.com>
2022-05-28 08:56:49 +0000

    700 二叉搜索树中的搜索

---
+++ leetcode/64/700_search_BST.js
@@ -0,0 +1,33 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-28 13:49:23
+ * @LastEditTime: 2022-05-28 13:49:26
+ * @Description: 700 二叉搜索树中的搜索
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+    // 二叉搜索树(排序树)
+    if (root) {
+        // 节点值大于val，找小的值，则找节点的左子节点
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        }
+    }
+    return root;
+};
