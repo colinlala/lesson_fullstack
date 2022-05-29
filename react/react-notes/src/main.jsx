bfb7cc81858123ab2e6788a0d7ccd93aabe64fa9
colincclala <1522302196@qq.com>
2022-05-29 14:53:00 +0000

    样式库引入 放在根上 import 'semantic-ui-css/semantic.min.css'

---
+++ react/react-notes/src/main.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-25 19:20:47
+ * @LastEditTime: 2022-05-25 20:12:42
+ * @Description: 
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+// 样式库引入 放在根上
+import 'semantic-ui-css/semantic.min.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
