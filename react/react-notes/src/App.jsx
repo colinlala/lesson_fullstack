41bb5521b0ef1202aca30bb047fbf136f5e0b26b
colincclala <1522302196@qq.com>
2022-05-29 14:53:20 +0000

    1

---
+++ react/react-notes/src/App.jsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-25 19:20:47
+ * @LastEditTime: 2022-05-25 20:12:28
+ * @Description: 
+ */
+import { useState } from 'react'
+import Notes from './components/Notes'
+// 样式库引入 放在根上
+// import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return <Notes />
+}
+
+export default App
