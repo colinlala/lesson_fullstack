d7febc00a844ce063a7e26628fa00f982ac35c2c
colincclala <1522302196@qq.com>
2022-05-30 14:58:55 +0000

    Header组件

---
+++ react/react-beers/src/Header.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-05-28 19:30:22
+ * @LastEditTime: 2022-05-28 19:31:10
+ * @Description: 
+ */
+import React from "react";
+
+
+const Header = ({ siteName }) => {
+    return (
+        <h1>
+            {siteName}
+        </h1>
+    )
+}
+
+export default Header
