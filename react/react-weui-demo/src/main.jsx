6b778c7b6b3f57345e970b0dfe6914f5efb7cd3a
colincclala <1522302196@qq.com>
2022-06-01 14:24:51 +0000

    1

---
+++ react/react-weui-demo/src/main.jsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-06-01 10:54:23
+ * @LastEditTime: 2022-06-01 12:16:14
+ * @Description: 
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'weui'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
