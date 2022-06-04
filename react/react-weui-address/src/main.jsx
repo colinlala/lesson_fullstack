fd892528c435ea5a0403db7575c0c673802aa7b3
colincclala <1522302196@qq.com>
2022-06-04 10:05:49 +0000

    import 'react-weui/build/packages/react-weui.css' 引入UI

---
+++ react/react-weui-address/src/main.jsx
@@ -0,0 +1,19 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-06-04 13:49:25
+ * @LastEditTime: 2022-06-04 15:29:55
+ * @Description: react-weui.css
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'weui'
+import 'react-weui/build/packages/react-weui.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <App />
+
+)
