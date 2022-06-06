a24696d8a2bc99663b1660fa730558b8b2ce71cf
colincclala <1522302196@qq.com>
2022-06-06 14:12:54 +0000

    1

---
+++ react/router-demo/router-code/src/About.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-06-06 19:52:14
+ * @LastEditTime: 2022-06-06 19:52:20
+ * @Description: 
+ */
+import { useState } from 'react'
+
+const About = () => {
+    return (
+        <>
+            About
+        </>
+    )
+}
+
+
+export default About
---
+++ react/router-demo/router-code/src/Home.jsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: ZYH
+ * @Email: 1522302196@qq.com
+ * @GiteeId: colincclala
+ * @Date: 2022-06-06 19:50:55
+ * @LastEditTime: 2022-06-06 19:51:24
+ * @Description: 
+ */
+import { useState } from 'react'
+
+const Home = () => {
+    return (
+        <>
+            Home
+        </>
+    )
+}
+
+
+export default Home
