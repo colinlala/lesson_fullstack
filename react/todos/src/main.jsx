824d36c383b1e95a2f45b8016322be147d26ec38
colincclala <1522302196@qq.com>
2022-06-09 15:44:56 +0000

    1

---
+++ react/todos/src/main.jsx
@@ -0,0 +1,16 @@
+/*
+ * @Date         : 2022-06-08 15:06:53
+ * @LastEditors  : colinlala
+ * @LastEditTime : 2022-06-08 19:55:04
+ * @description  : 
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import './styles/style.css'
+import './styles/reset.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
