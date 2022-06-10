ada246b571636d23af8ff1f62511a8aa502d6c30
colincclala <1522302196@qq.com>
2022-06-10 12:10:36 +0000

    1

---
+++ react/react-weui-router-demo/src/pages/detail.jsx
@@ -0,0 +1,18 @@
+/*
+ * @Date         : 2022-06-10 14:57:42
+ * @LastEditors  : colinlala
+ * @LastEditTime : 2022-06-10 15:15:56
+ * @description  : detail详情页面
+ */
+// Route path -> element  detail详情页面
+import React from "react";
+
+const Detail =()=>{
+    return (
+        <div>
+            Detail
+        </div>
+    )
+}
+
+export default Detail
---
+++ react/react-weui-router-demo/src/pages/login.jsx
@@ -0,0 +1,19 @@
+/*
+ * @Date         : 2022-06-10 14:58:40
+ * @LastEditors  : colinlala
+ * @LastEditTime : 2022-06-10 15:14:52
+ * @description  : 登录页
+ */
+// 登录页
+
+import React from "react";
+
+const Login =()=>{
+    return (
+        <div>
+            Login
+        </div>
+    )
+}
+
+export default Login
