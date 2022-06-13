0c3edce411b9832f301e38ea9f170f36717813d7
colincclala <1522302196@qq.com>
2022-06-13 14:09:20 +0000

    样式组件

---
+++ react/movie-react/components/contest/style.js
@@ -0,0 +1,24 @@
+/*
+ * @Date         : 2022-06-13 20:36:08
+ * @LastEditors  : colinlala
+ * @LastEditTime : 2022-06-13 21:56:49
+ * @description  : 
+ */
+// css in js 
+import styled from 'style-components'
+
+export const ContestWrapper = styled.div`
+    height:50px;
+    background-color:blue;
+    h2{
+        font-size:20px;
+    }
+`
+export const Tab = styled.ul`
+
+`
+   
+export const TabItem = styled.li`
+`
+
+export const NoData = styled.div``
