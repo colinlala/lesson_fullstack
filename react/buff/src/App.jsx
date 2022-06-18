1f66f8aa6ef7ed9dda190615132a3528a9f0d547
colincclala <1522302196@qq.com>
2022-06-18 10:53:40 +0800

    建立buff框架

---
+++ react/buff/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Find from './pages/Find'
+import Repertory from './pages/Repertory'
+import Sell from './pages/Sell'
+import Mine from './pages/Mine'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/repertory' element={<Repertory />}></Route>
+        <Route path='/find' element={<Find />}></Route>
+        <Route path='/mine' element={<Mine />}></Route>
+        <Route path='/sell' element={<Sell />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ react/buff/src/config/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Date         : 2022-06-16 16:35:06
+ * @LastEditors  : colinlala
+ * @LastEditTime : 2022-06-16 20:33:59
+ * @description  : 
+ */
+// 页面标题配置
+export const pageTitle = {
+    '/':'首页',
+    '/home':'首页',
+    '/repertory':'库存',
+    '/find':'发现',
+    '/mine':'我的',
+    '/sell':'出售',
+}
