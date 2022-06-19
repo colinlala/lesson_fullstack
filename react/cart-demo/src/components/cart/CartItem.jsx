6adcabceb9a8509eeeea5c26f9278286db2da5d5
colincclala <1522302196@qq.com>
2022-06-19 22:41:10 +0800

    购物车总计勾选

--- react/cart-demo/src/components/cart/CartItem.jsx
+++ react/cart-demo/src/components/cart/CartItem.jsx
@@ -2,8 +2,12 @@ import { Typography } from 'antd'
 import React from 'react'
 
 export default function CartItem(props) {
-    const { item } = props
+    const { item, onCheckedChange } = props
     const { checked, name, price } = item
+    // 为什么还要去声明一个函数？
+    // const onWrapCheckedChange = (e) => {
+    //     onCheckedChange(item)
+    // }
 
     return (
         <div className="item-card">
@@ -11,6 +15,8 @@ export default function CartItem(props) {
                 <input
                     type="checkbox"
                     checked={checked}
+                    // 如果没有别的事情处理，就最好这样写
+                    onChange={onCheckedChange.bind(null,item)}
                 />
             </div>
             <p className="item-info">
--- react/cart-demo/src/components/cart/index.jsx
+++ react/cart-demo/src/components/cart/index.jsx
@@ -15,16 +15,40 @@ const Cart = () => {
     )
     const [total, setTotal] = useState(0)
     const [checkedAll, setCheckedAll] = useState(false)
-    const onCheckedChange = () => {
-
+    const onCheckedChange = (item) => {
+        // console.log(item, '--------------')
+        // 选中了checked为true
+        let idx = cartData.findIndex(data => item.id === data.id)
+        // console.log(idx)
+        cartData[idx].checked = !cartData[idx].checked
+        setCartData([
+            ...cartData
+        ])
     }
     const onWrapCheckedAllChange = () => {
-
+        // setCheckedAll(!checkedAll)
+        // 全选
+        let newData = cartData.map(item => {
+            item.checked = !checkedAll
+            return item
+        })
+        setCartData([
+            ...newData
+        ])
     }
     useEffect(() => {
         // mounted  挂载
         // update
-
+        // 点击就打印
+        // console.log('?????')
+        // item.checked为true会被筛选，筛选完后还是数组,reduce后把数组转为数值
+        let totalPrice = cartData
+            .filter(item => item.checked)
+            .reduce((prev, item) => {
+                return prev + item.price
+            }, 0)
+        setTotal(totalPrice)
+        setCheckedAll(cartData.every(item => item.checked))
     }, [cartData])
 
     const Footer = (
