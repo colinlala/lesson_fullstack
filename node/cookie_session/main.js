6f1edd8488cd068c45eacbd6d58cbf968e3c32d0
colincclala <1522302196@qq.com>
2022-07-20 23:18:26 +0800

    session

--- node/cookie_session/main.js
+++ node/cookie_session/main.js
@@ -2,17 +2,79 @@
 // react 单页应用 MVVM 模式的项目  Model View ViewModel
 // 后端 模式？ MVC Model（数据模型，model）  View（html） Controller
 // Model View 不能直接通信    要通过Controller来管理（业务）
-const Koa = require("koa"); // 最好用的node 后端框架，遵守mvc开发范式
+const Koa = require("koa"); // 第三方 最好用的node 后端框架，遵守mvc开发范式
 const app = new Koa(); // 意思是把 web server 理解为 server app
+const path = require("path"); // 内置
+// 支持静态服务的中间件
+const staticServer = require("koa-static");
+const koaSession = require("koa-session"); // 中间件
+const session_signed_key = ["secret"]; // key
+
+app.keys = session_signed_key;
+const session_config = {
+    key: "koa:sess" /**  cookie的key。 (默认是 koa:sess) */,
+    maxAge: 1000 * 60 * 60/**  session 过期时间，以毫秒ms为单位计算 。*/,
+    autoCommit: true /** 自动提交到响应头。(默认是 true) */,
+    overwrite: true /** 是否允许重写 。(默认是 true) */,
+    httpOnly: true /** 是否设置HttpOnly，如果在Cookie中设置了"HttpOnly"属性，那么通过程序(JS脚本、Applet等)将无法读取到Cookie信息，这样能有效的防止XSS攻击。  (默认 true) */,
+    signed: true /** 是否签名。(默认是 true) */,
+    rolling: true /** 是否每次响应时刷新Session的有效期。(默认是 false) */,
+};
+const session = koaSession(session_config, app);
+
+// 使用中间件，注意有先后顺序
+app.use(session); // 启用session
 // use 启用一个中间件  函数
 // app.use 就好像 redux compose
-// ctx context 
+// ctx context
+
+// 静态服务器 js css img file   就不需要声明路由
+// 中间件有顺序，数组，compose
+app.use(staticServer(path.join(__dirname, "./static")));
+// req -> res  applyMiddleware 挂载一个中间件
+// ctx = req + res
 app.use(async ctx => {
+    // if (ctx.req.url == )
+    console.log(ctx.path, '--------------')
+    if (ctx.path == '/login') {
+        // 登录？ 
+        const { userName, password } = ctx.query;
+        // console.log(userName, password, '---------');
+        if (userName == 'test' && password == 'test') {
+            ctx.session.login = {
+                name: userName,
+                uid: 2,
+                sex: '男',
+                level: 2
+              }
+            ctx.type = 'html'
+            ctx.body = `
+            登录成功
+            <a href="/testlogin">去测试登录</a>
+            `;
+        }
+    } else if (ctx.path == '/testlogin') {
+        console.log(ctx.session, '/////');
+        if (!ctx.session.login.name) {
+            ctx.redirect('/');
+        }
+        else {
+            ctx.body = '已经登录' + ctx.session.login.name;
+        }
+    }
+
+    // 后端种了一个uid的cookie
+    // Domain + path 是对cookie的一种限定   HttpOnly提高安全性 设置为false，通过document.cookie就可以获取cookie
+    // ctx.cookies.set('uid','123456',{
+    //     path: '/admin',
+    //     maxAge: 1000 * 10 * 60,
+    //     httpOnly: true,     // 默认为 true
+    // })
+    // 动态数据
     // 支持异步
-    // 数据库的查询  
+    // 数据库的查询
     // await db.find()
-    ctx.body = `Hello World`;
+    // ctx.body = `Hello World`;
 });
 // node main.js后   在loalhost：1314可以运行
 app.listen(1314);
-
--- node/cookie_session/readme.md
+++ node/cookie_session/readme.md
@@ -28,3 +28,32 @@
         - 服务器IP  交给相应的进程   通过url解析端口 ， 端口 -> 进程（映射）
         - web server 的 createServer callback 执行   res.end('')返回结果
         - res解析出A的IP ，拿到返回结果
+
+
+- `npx nodemon main.js` npx直接运行node_moduled里面的nodemon，然后localhost：1314/logo.jpeg 就可以访问静态资源
+
+- node 和 redux 实现功能的思路有点像
+    - 中间件思想，启用中间件为应用提供功能
+    - 第三方的中间件，**koa-static**，静态资源服务器的搭建
+        - `app.use(staticServer(path.join(__dirname,'./static')))`
+        - 这个静态资源会自动去做缓存
+    ```js
+        app.use(async ctx => {
+           // 最普通的中间件 
+        })
+    ```
+
+- httpOnly  安全的设置 cookie属性
+    - true  js 不可以读取，  避免 XSS 跨站脚本攻击
+    - false  设置  主题设置...没有重要的东西
+
+- session ? 
+    - 又一种识别用户身份状态的
+    - cookie 不够用了（4kb），才又来一个session
+    - cookie 每次请求都会带上，**性能低**，速度就慢了，
+    - cookie 是在每次请求中带上的，客户端和服务器端都有状态标记
+        - uid 不足够识别用户，用户表有很多字段，不够用
+        - 在内存中，为每一个访问用户，提供一个状态存储的空间，比cookie 会从容一些（空间更大），方便业务展开，如 uid关联外键...
+    - session 存储表示用户状态，只在后端内存中
+        - cookie 带上 uid 传到服务器端来了，通过uid这个钥匙到session（key value） 内存表数据表，以后要用直接拿出来
+    - session 基于 cookie
